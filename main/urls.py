1693b6043ab118f1f0c29648ce61f89fb7483442
Your Name <rycx0121@qq.com>
2019-03-05 11:22:45 +0800

    Admin里添加Markdown编辑器

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,4 @@
 """main URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
@@ -16,8 +15,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import markdown_uploader
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
+    path('martor/', include('martor.urls')),
+    path('api/uploader/', markdown_uploader, name='markdown_uploader_page'),
 ]
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.db import models
+from martor.widgets import AdminMartorWidget
+from .models import Article, Tag, ArticleTags
+
+
+class TagsInlineAdmin(admin.StackedInline):
+    model = Article.tags.through
+    extra = 0 # 去掉空行
+    can_delete = True # 让文章关联的标签可以移除
+
+class ArticleAdmin(admin.ModelAdmin):
+    # save_on_top = True
+    fieldsets = [
+        (None, {'fields': ['title', 'content', 'create_time', 'upate_time']}),
+    ]
+    readonly_fields = ['create_time', 'upate_time'] # 只读字段
+    # show_change_link = True
+    inlines = (TagsInlineAdmin,)
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Tag)
+# admin.site.register(ArticleTags)
